2035ab4e33026fa90ad7139452ab4ab2022f2805
Loosekonnection <david.bates@loosekonnection.co.uk>
2021-02-20 16:55:26 +0000

    added copyright section & code refator

--- index.js
+++ index.js
@@ -9,39 +9,38 @@ const prompts = () =>
     {
         name: 'title',
         type: 'input',
-        message: 'Please enter the title of your project?',
+        message: '*Required - Enter the title of your project:',
         validate: (answer) => {
             if (answer) {
                 return true;
             } else {
-                return 'You need to enter a valid project title.';
+                return 'You must enter a project title.';
             }
         }
     },
     {
         name: 'description',
         type: 'input',
-        message: 'Please write a description of your project.',
+        message: '*Required - Write a description of your project:',
         validate: (answer) => {
             if (answer) {
                 return true;
             } else {
-                return 'You need to enter a valid project description.';
+                return 'You must enter a project description.';
             }
         }
     },
     {
         name: 'installation',
         type: 'input',
-        message: 'Please describe the install procedure of your project for the installation instructions.',
+        message: 'Describe the install procedure of your project for the installation instructions.',
     },
     {
         name: 'usage',
         type: 'input',
-        message: 'Please describe how to use your project for the Usage section.',
+        message: 'Describe how to use your project for the Usage section.',
     },
     {
-        
         name: 'contributing',
         type: 'input',
         message: 'Provide detail on how other developers can contribute to your project.',
@@ -49,12 +48,12 @@ const prompts = () =>
     {
         name: 'tests',
         type: 'input',
-        message: 'Provide detail on any tests written for your application and any examples on how to run the tests.',
+        message: 'Provide details on any tests written for your application and examples of how to run the tests.',
     },
     {
         name: 'license',
         type: 'list',
-        message: 'Select the license that is associated with your project?',
+        message: 'Select the license that is associated with your project:',
         choices: [
             'Apache License 2.0',
             'GNU General Public License v3.0',
@@ -70,18 +69,11 @@ const prompts = () =>
             'Mozilla Public License 2.0',
             'The Unlicense'
         ],
-        validate: (answer) => {
-            if (answer) {
-                return true;
-            } else {
-                return 'Please enter a valid license type.';
-            }
-        }
     },
     {
         name: 'username',
         type: 'input',
-        message: 'What is your GitHub username?',
+        message: '*Required - Enter your GitHub username, without the @ symbol?',
         validate: (answer) => {
             if (answer) {
                 return true;
@@ -93,7 +85,7 @@ const prompts = () =>
     {
         name: 'email',
         type: 'input',
-        message: 'What is your email address?',
+        message: '*Required - What is your preferred contact email address?',
         validate: (answer) => {
             if (answer) {
                 return true;
@@ -101,6 +93,16 @@ const prompts = () =>
                 return 'You need to enter a valid email address.';
             }
         }
+    },
+    {
+        name: 'copydate',
+        type: 'number',
+        message: 'For the Copyright, Enter the current year:',
+    },
+    {
+        name: 'copymsg',
+        type: 'input',
+        message: 'Enter a copyright message:',
     },
 ]);
 
@@ -118,6 +120,7 @@ function writeToFile(fileName, data) {
 function init() {
 
     prompts().then((answers) => {
+        console.log(answers);
 
         writeToFile('README.md', answers);
     });   
